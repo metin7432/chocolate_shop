45a59abf580c6140d5f8627b45c310d37f050dae
metin7432 <metin7432@gmail.com>
2025-11-25 18:53:03 +0300

    frontend start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {Route, Routes}  from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+
+function App() {
+ 
+
+  return (
+    <div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+function HomePage()  {
+
+    return (
+        <div>
+        Heloooo
+        </div>
+    )
+}
+
+export default HomePage

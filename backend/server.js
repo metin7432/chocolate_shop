f69f72dc4558bdc33bc26d5e71f660b016c565ce
Metin altay <39528623+metin7432@users.noreply.github.com>
2026-02-18 21:56:25 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/api/coupons", couponRouters);
 app.use("/api/payments", paymentRouters);
 app.use("/api/analytics", analyticsRouters);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req,res)=> {

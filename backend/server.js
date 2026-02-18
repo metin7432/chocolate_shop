230639213691d146364ca3d09d62acafcccd06a4
Metin altay <39528623+metin7432@users.noreply.github.com>
2026-02-18 21:58:41 +0300

    Serve static files in non-production environment
    Serve static files and handle all routes in non-production.

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,13 @@ app.use("/api/coupons", couponRouters);
 app.use("/api/payments", paymentRouters);
 app.use("/api/analytics", analyticsRouters);
 
-if (process.env.NODE_ENV !== "production") {
+
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req,res)=> {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
-}
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port} in active`);
     connectDB()
